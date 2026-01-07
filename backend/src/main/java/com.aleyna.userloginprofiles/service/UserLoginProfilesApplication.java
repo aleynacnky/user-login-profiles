16d0a166604ba8f10c7438b0674d919d82d11300
Aleyna Çinkaya <aleynacinkaya1@gmail.com>
2026-01-07 16:43:21 +0300

    Add files via upload

---
+++ backend/src/main/java/com.aleyna.userloginprofiles/service/UserLoginProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.aleyna.userloginprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserLoginProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserLoginProfilesApplication.class, args);
+	}
+
+}
